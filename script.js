8f4cad8ac48868f71c1446f00c339de7e22168b4
Victor Nascimento | DevOps Spec <victorf_16@hotmail.com>
2025-01-23 23:22:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+async function run() {
+  const response = await fetch('assets/config.json');
+  const json = await response.json();
+  Object.entries(json).forEach(([key, value]) => {
+    const el = document.querySelector(`#${key}`);
+    if (el) {
+      el.textContent = value;
+    } else {
+      console.warn(`Element with id : ${key} not found ...`);
+    }
+  });
+}
+
+run().catch((err) => {
+  console.error(err);
+});
