0d3a5469ecd909efde1c75ea4c55b37fd9b7d4cd
Victor Nascimento | DevOps Spec <victorf_16@hotmail.com>
2025-01-27 23:09:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ async function run() {
   const response = await fetch('assets/config.json');
   const json = await response.json();
   Object.entries(json).forEach(([key, value]) => {
-    const el = document.querySelector(`#${key}`);
+    const el = document.querySelector('#${key}');
     if (el) {
       el.textContent = value;
     } else {
-      console.warn(`Element with id : ${key} not found ...`);
+      console.warn('Element with id : ${key} not found ...');
     }
   });
 }
